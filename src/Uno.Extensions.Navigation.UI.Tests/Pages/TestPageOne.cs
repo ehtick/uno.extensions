3e42c2b146fb0c0a3950d275422e453335e4031c
Steve Bilogan <steve.bilogan@gmail.com>
2026-02-16 17:16:11 -0500

    fix: adjust routenotifier logic

---
+++ src/Uno.Extensions.Navigation.UI.Tests/Pages/TestPageOne.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace Uno.Extensions.Navigation.UI.Tests.Pages;
+
+public sealed class TestPageOne : Page
+{
+	public TestPageOne()
+	{
+		Content = new TextBlock { Text = "Test Page One" };
+	}
+}
